ba42fa71c8794cad67a0a181b38585a975cb8291
grafst <grafst@users.noreply.github.com>
2014-08-18 00:23:51 +0200

    Unterstützung für mehrere Dictionary Files hinzugefuegt

--- functions/istext.php
+++ functions/istext.php
@@ -4,23 +4,27 @@ function istext ($text)
 {
 	//Anzahl gefundene Woerter
 	$wordcount=0;
-	// Dictionary oeffnen
-	$handle = @fopen("german.dic", "r");
-	
-	if ($handle) {
-		//Woerterbuch Zeilenweise (Wort fuer Wort) einlesen
-		while (($wort = fgets($handle, 4096)) !== false) {
-			//Newline Charakter am ende des Wortes entfernen
-			$wort = rtrim($wort, "\r\n");
-			//Wort im Text suchen
-			if (stripos("x".$text,$wort) >0) {
-				$wordcount++;
+	// Dictionaries suchen
+	$dictionaries=glob(realpath(dirname(__FILE__))."/*.dic");
+	foreach ($dictionaries as $dictionary) {
+		echo $dictionary;
+		//Dictionary oeffnen
+		$handle = @fopen($dictionary, "r");
+		if ($handle) {
+			//Woerterbuch Zeilenweise (Wort fuer Wort) einlesen
+			while (($wort = fgets($handle, 4096)) !== false) {
+				//Newline Charakter am ende des Wortes entfernen
+				$wort = rtrim($wort, "\r\n");
+				//Wort im Text suchen
+				if (stripos("x".$text,$wort) >0) {
+					$wordcount++;
+				}
 			}
+			if (!feof($handle)) {
+				echo "Error: unexpected fgets() fail\n";
+			}
+			fclose($handle);
 		}
-		if (!feof($handle)) {
-			echo "Error: unexpected fgets() fail\n";
-		}
-		fclose($handle);
 	}
 	return $wordcount;
 }
