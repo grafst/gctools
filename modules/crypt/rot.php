61d006529fc732aab97c1190048e3c6712d82f09
Jonas Meier <meiers.jonas@gmail.com>
2014-08-17 16:23:18 +0200

    add input form

--- modules/crypt/rot.php
+++ modules/crypt/rot.php
@@ -5,6 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	//do the rotate
 } else {
 	//show the form to enter a text
+	echo '<form action="/modules/crypt/rot.php" method="post"><textarea name="string"</textarea></form>';
 }
 
 
