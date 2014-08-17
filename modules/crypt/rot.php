6600b7547c2d85a9ec7a6217c1324d8daed41598
Jonas Meier <meiers.jonas@gmail.com>
2014-08-17 16:35:50 +0200

    fix of fix c7b588fda227d901c49a523ede315ea19fd909b3

--- modules/crypt/rot.php
+++ modules/crypt/rot.php
@@ -4,7 +4,7 @@ include '../../functions/rot.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	//do the rotate
 	echo '<table>';
-	$input = $_POST['sting'];
+	$input = $_POST['string'];
 	for ($i = 1; $i < 26 ; $i++) {
 		$outputstring = rot("$i","$inputstring");
 		echo "<tr><td>Rot $i</td><td>$output</td></tr>";
